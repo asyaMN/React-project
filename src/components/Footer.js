1b5fcd934e8439e5f2b83605c6d1d9cd43bd3f31
asyaMN <asia.nuairat@gmail.com>
2021-11-14 11:44:39 +0200

    new

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div>
+                <h4>All Rights Reserved &copy; Asya M Nuairat </h4>
+            </div>
+        )
+    }
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React ,{Component}from "react";
+
+
+
+class Header extends Component{
+    render(){
+        return(
+            <h1 className='h1' >Horned Beast</h1>
+            
+        );
+    }
+}
+
+export default Header;
